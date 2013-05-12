2c05b274cf2868f3237496631ffa8ba593f6ae20
Jarrell Waggoner <malloc47@gmail.com>
2013-05-11 21:07:18 -0400

    added tick support (use a second argument in the 0.001 to 0.01 range)

--- befunge.py
+++ befunge.py
@@ -5,7 +5,8 @@ import befunge.interpreter
 def main(*args):
     filename = args[1]
     state = befunge.interpreter.init_std_befunge_state(filename)
-    befunge.interpreter.run(state)
+    wait = float(args[2]) if len(args)>2 else 0
+    befunge.interpreter.run(state,wait=wait)
 
 if __name__ == '__main__':
     sys.exit(main(*sys.argv))
