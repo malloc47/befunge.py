6134cc9d1c6718df74664da6bf41551b4902e678
Jarrell Waggoner <malloc47@gmail.com>
2013-05-12 03:27:42 -0400

    pep8'd and pyflake'd

--- befunge.py
+++ befunge.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
-import sys,os
+import sys
 import befunge
+
 
 def main(*args):
     filename = args[1]
     state = befunge.init_std_befunge_state(filename)
-    wait = float(args[2]) if len(args)>2 else 0
-    output = befunge.run(state,wait=wait,display=True)
+    wait = float(args[2]) if len(args) > 2 else 0
+    befunge.run(state, wait=wait, display=True)
+    # output = befunge.run(state, wait=wait, display=True)
     # print(output)
+
 
 if __name__ == '__main__':
     sys.exit(main(*sys.argv))
